e66e036896621a7183f9e0bc61211d63ecae3f07
Tristan Russell <tristanrussell2000@gmail.com>
2025-06-14 18:38:27 -0700

    Chapter 15 + Challenge 4

--- main.c
+++ main.c
@@ -1,19 +1,35 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+	initVM();
 	Chunk chunk;
 	initChunk(&chunk);
 
 	int constant = addConstant(&chunk, 1.2);
-	int constant2 = addConstant(&chunk, 3.14);
 	writeChunk(&chunk, OP_CONSTANT, 123);
 	writeChunk(&chunk, constant, 123);
+
+	constant = addConstant(&chunk, 3.14);
 	writeChunk(&chunk, OP_CONSTANT, 123);
-	writeChunk(&chunk, constant2, 123);
+	writeChunk(&chunk, constant, 123);
+
+	writeChunk(&chunk, OP_ADD, 123);
+
+	constant = addConstant(&chunk, 5.6);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+
+	writeChunk(&chunk, OP_DIVIDE, 123);
+	writeChunk(&chunk, OP_NEGATE, 123);
+
 	writeChunk(&chunk, OP_RETURN, 124);
-	disassembleChunk(&chunk, "test chunk");
+
+	interpret(&chunk);
+
+	freeVM();
 	freeChunk(&chunk);
 	return 0;
 }
