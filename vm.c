12e5ef68503f7358fff603457824ea8c0ca59888
Tristan Russell <tristanrussell2000@gmail.com>
2025-06-25 11:13:11 -0700

    Put IP in register local variable

--- vm.c
+++ vm.c
@@ -11,6 +11,7 @@
 
 VM vm;
 
+
 static Value clockNative(int argCount, Value* args) {
 	return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
 }
@@ -136,11 +137,12 @@ static void concatenate(void) {
 
 static InterpretResult run(void) {
 	CallFrame* frame = &vm.frames[vm.frameCount - 1];
+	register uint8_t *ip = frame->ip;
 
-#define READ_BYTE() (*frame->ip++)
+#define READ_BYTE() (*ip++)
 #define READ_CONSTANT() (frame->function->chunk.constants.values[READ_BYTE()])
 #define READ_SHORT() \
-	(frame->ip += 2, (uint16_t)((frame->ip[-2] << 8) | frame->ip[-1]))
+	(ip += 2, (uint16_t)((ip[-2] << 8) | ip[-1]))
 #define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op) \
 	do { \
@@ -253,17 +255,17 @@ static InterpretResult run(void) {
 			}
 			case OP_JUMP: {
 				uint16_t offset = READ_SHORT();
-				frame->ip += offset;
+				ip += offset;
 				break;
 			}
 			case OP_JUMP_IF_FALSE: {
 				uint16_t offset = READ_SHORT();
-				if (isFalsey(peek(0))) frame->ip += offset;
+				if (isFalsey(peek(0))) ip += offset;
 				break;
 			}
 			case OP_LOOP: {
 				uint16_t offset = READ_SHORT();
-				frame->ip -= offset;
+				ip -= offset;
 				break;
 			}
 			case OP_CALL: {
@@ -271,7 +273,9 @@ static InterpretResult run(void) {
 				if (!callValue(peek(argCount), argCount)) {
 					return INTERPRET_RUNTIME_ERROR;
 				}
+				frame->ip = ip;
 				frame = &vm.frames[vm.frameCount - 1];
+				ip = frame->ip;
 				break;
 			}
 			case OP_RETURN: {
@@ -284,6 +288,7 @@ static InterpretResult run(void) {
 				vm.stackTop = frame->slots;
 				push(result);
 				frame = &vm.frames[vm.frameCount - 1];
+				ip = frame->ip;
 				break;
 			}
 		}
